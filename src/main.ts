b49e154252d357e8ccd78c1617978e95fcd6a1db
Daniel Flores Hurtado <daniel.hurtado4774@gmail.com>
2023-07-12 15:51:28 -0500

    Changed Port and detached remote form trainer

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  const port = 4231;
+  const port = 443;
   await app.listen(port, () => {
     logger.log(`Application listening on port ${port}`);
   });
